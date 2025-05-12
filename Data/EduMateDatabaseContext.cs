e371a19fe53b68462c31d110513eddf57d68b259
nodairyco <aniashvili.nodar@kiu.edu.ge>
2025-05-12 04:04:17 +0400

    Added role entity, role table and UserRoles relationship.

--- Data/EduMateDatabaseContext.cs
+++ Data/EduMateDatabaseContext.cs
@@ -12,6 +12,19 @@ public class EduMateDatabaseContext(DbContextOptions<EduMateDatabaseContext> opt
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.HasSequence<int>("roleidsequence")
+            .StartsAt(1)
+            .IncrementsBy(1);
+        
+        modelBuilder.Entity<Roles>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Id)
+                .HasDefaultValueSql("nextval('roleidsequence')")
+                .ValueGeneratedOnAdd();
+        });
+        
+        
         modelBuilder.Entity<User>(entity =>
         {
             entity.Property(u => u.Email)
@@ -28,6 +41,10 @@ public class EduMateDatabaseContext(DbContextOptions<EduMateDatabaseContext> opt
                 .IsRequired();
             //Creating Id as primary key
             entity.HasKey(u => u.Id);
+            
+            entity.HasMany(u => u.Roles)
+                .WithMany(r => r.Users)
+                .UsingEntity(j => j.ToTable("UserRoles"));
         });
 
         modelBuilder.Entity<Followers>(e =>
@@ -45,23 +62,26 @@ public class EduMateDatabaseContext(DbContextOptions<EduMateDatabaseContext> opt
     }
 }
 
+public class Roles
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+
+    [JsonIgnore] public virtual ICollection<User> Users { get; set; } = new List<User>();
+}
+
 public class User
 {
     public Guid Id { get; set; } = Guid.NewGuid();
-    [MinLength(8)]
-    [MaxLength(255)]
-    public string Username { get; set; } = string.Empty;
-    [MaxLength(255)]
-    public string Email { get; set; } = string.Empty;
-    [MinLength(8)]
-    [MaxLength(255)]
-    public string Password { get; set; } = string.Empty;
+    [MinLength(8)] [MaxLength(255)] public string Username { get; set; } = string.Empty;
+    [MaxLength(255)] public string Email { get; set; } = string.Empty;
+    [MinLength(8)] [MaxLength(255)] public string Password { get; set; } = string.Empty;
 
-    [JsonIgnore]
-    public virtual ICollection<Followers> Following { get; set; } = new List<Followers>();
-   
-    [JsonIgnore]
-    public virtual ICollection<Followers> Followers { get; set; } = new List<Followers>();
+    [JsonIgnore] public virtual ICollection<Roles> Roles { get; set; } = new List<Roles>();
+
+    [JsonIgnore] public virtual ICollection<Followers> Following { get; set; } = new List<Followers>();
+
+    [JsonIgnore] public virtual ICollection<Followers> Followers { get; set; } = new List<Followers>();
 }
 
 public class Followers
